a64b774bc7469f98b1dffa9d2784291c9775e81c
Mangesh Bhardwaj <mangeshbhardwaj007@gmail.com>
2026-01-05 17:56:44 +0530

    updated layout page and added support for current search city in dashboard page

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,5 +1,13 @@
-import { ComponentExample } from "@/components/component-example";
+"use client";
+
+import { useSearchParams } from "next/navigation";
 
 export default function Page() {
-return <ComponentExample />;
+  const params = useSearchParams();
+  const cityId = params.get("city");
+  return (
+    <main>
+      <h1 className="text-8xl text-black">{cityId}</h1>
+    </main>
+  );
 }
