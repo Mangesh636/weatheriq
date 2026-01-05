6281c27148a1098d7b9f83e13e3c3024446e4511
Mangesh Bhardwaj <mangeshbhardwaj007@gmail.com>
2026-01-05 17:52:23 +0530

    added support for private env and exported

--- config/env.ts
+++ config/env.ts
@@ -1,16 +1,35 @@
-import {z} from "zod"
+import { z } from "zod"
 
 const publicEnvSchema = z.object({
     NEXT_PUBLIC_BASE_URL: z.url(),
 })
 
-const publicEnvSchemaResult = publicEnvSchema.safeParse(process.env)
+const privateEnvSchema = z.object({
+    GEOCODING_API_URL: z.url(),
+    FORECAST_API_URL: z.url(),
+})
 
-if (!publicEnvSchemaResult.success) {
+const publicEnvSchemaResult = publicEnvSchema.safeParse(process.env)
+const privateEnvSchemaResult = privateEnvSchema.safeParse(process.env)
+
+if (!publicEnvSchemaResult.success || !privateEnvSchemaResult.success) {
     console.error("Env validation failed");
 
-    if(!publicEnvSchemaResult.success) {
+    if (!publicEnvSchemaResult.success) {
         console.error("Public env validation failed");
         console.error(publicEnvSchemaResult.error.issues);
     }
+
+    if (!privateEnvSchemaResult.success) {
+        console.error("Private env validation failed");
+        console.error(privateEnvSchemaResult.error.issues);
+    }
+
+    // Exit the process if env validation fails
+    process.exit(1);
+}
+
+export const env = {
+    ...publicEnvSchemaResult.data,
+    ...privateEnvSchemaResult.data,
 }
